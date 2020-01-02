45c7e4e831fcd7f099bace6f7ccf5b5423920953
zenen jaimes <slasherxf@gmail.com>
2020-01-02 00:59:17 -0800

    Implemented int and long arrays. Also changed byte array to not return raw byte array

--- src/nbt_tools/nbt/int_array.py
+++ src/nbt_tools/nbt/int_array.py
@@ -6,14 +6,14 @@ def byte_length() -> int:
 def read(info, buf, mutdata):
     data = buf.read(byte_length())
     _size = (data[0] << 24) | (data[1] << 16) | (data[2] << 8) | data[3]
-    #fresh_bytes = buf.read(_size)
+    fresh_bytes = buf.read(_size * 4)
 
     tags = []
     for i in range(_size):
         newdata = dict()
-        nbt.read_tag(buf, newdata, nbt.TAG.Byte.value)
+        nbt.read_tag(buf, newdata, nbt.TAG.Int.value)
         tags.append(newdata)
 
     values = list(map(lambda tag: tag['']['value'], tags))
 
-    return dict({'size': _size, 'raw': values, 'value': _size, 'size_bytes': 1, 'tags': tags})
+    return dict({'size': _size, 'raw': values, 'value': _size, 'size_bytes': 4, 'tags': tags})
