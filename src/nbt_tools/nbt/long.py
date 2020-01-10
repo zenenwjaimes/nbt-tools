9fabdca6438b6ded30b62536c82db77529805b15
zenen jaimes <slasherxf@gmail.com>
2020-01-10 03:58:45 -0800

    Long new structure being added

--- src/nbt_tools/nbt/long.py
+++ src/nbt_tools/nbt/long.py
@@ -9,3 +9,13 @@ def read(buf):
     data = buf.read(byte_length())
 
     return nbt.to_long(data)
+
+def write(data):
+    tag_header = nbt.get_tag_header(data)
+    res = b''.join([
+            nbt.get_tag_header(data),
+            bytes(data['tag_name'], 'utf-8'),
+            int(data['value']).to_bytes(byte_length(), byteorder='big', signed=True)
+    ])
+
+    return res 
